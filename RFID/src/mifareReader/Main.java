ab0b7d173f50d5e7a3a768a96b282979826dcd5e
misch <Init>
2013-05-14 16:58:09 +0200

    a

--- RFID/src/mifareReader/Main.java
+++ RFID/src/mifareReader/Main.java
@@ -11,6 +11,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		/**
+		 *  CHECK OS.ARCH
+		 */
+		
 		new ReaderGUI(new ReaderHandler());
 
 		
