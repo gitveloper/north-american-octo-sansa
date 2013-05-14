0360738ad6dc3fd3db7c33a38dc9dec3d699f779
Init <Init>
2013-05-14 16:41:09 +0200

    doing

--- RFID/src/mifareReader/Main.java
+++ RFID/src/mifareReader/Main.java
@@ -11,16 +11,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		if(System.getProperty("os.arch").contains("64")) {
-			System.setProperty( "java.library.path", "RFID/_rxtx/libs-32/" );
-			System.out.println("64 bit " + System.getProperty("os.arch"));
-			new ReaderGUI(new ReaderHandler());
-		} else {
-			System.out.println("BWHAHAHA");
-		}
-		
-		
+		new ReaderGUI(new ReaderHandler());
+
 		
 	}
 
