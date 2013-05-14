63abdf30677495772eba9ae187573e1d7076a653
Koerk <Koerk@193.197.206.106>
2013-05-14 16:52:52 +0200

    kommentar muss

--- RFID/src/datenbank/DBHandler.java
+++ RFID/src/datenbank/DBHandler.java
@@ -1,13 +1,52 @@
 package datenbank;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class DBHandler {
-
-	public DBHandler() {
+	
+	public Connection setConn(){
+		Connection conn = null;
+		
+		try{
+		
+		Class.forName("org.h2.Driver");
+        
+        conn = DriverManager.getConnection(
+                "jdbc:h2:D:/Programme/H2DB/database", "sa", "");
+        
+        
+		}catch (Exception e) {
+            e.printStackTrace();
+        }
+		return conn;
+	}
+	
+	public double addGuthaben(Connection conn, Double guth, Integer kundenNr){
+		double guthaben_neu = 0.00;
+		
+		try{
+			Statement stmt = conn.createStatement();
+			String getQuery = "SELECT Guthaben FROM Kunden WHERE KundenNr = "+kundenNr;
+			ResultSet selectRS = stmt.executeQuery(getQuery);
+			selectRS.next();
+			guthaben_neu = selectRS.getDouble(1)+guth;
+			
+			String addQuery = "UPDATE Kunden SET Guthaben ="+guthaben_neu+" WHERE KundenNR = kundenNr";
+			stmt.executeUpdate(addQuery);
+			
+						
+	        
+			}catch (Exception e) {
+	            e.printStackTrace();
+	        }
+			return guthaben_neu;
 		
 	}
 	
-	public DBHandler(DBConnection conn) {
-		
-	}
+	
 
 }
--- RFID/src/datenbank/Main.java
+++ RFID/src/datenbank/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new DBHandler(new DBConnection());
+		new DBHandler();
 	}
 
 }
